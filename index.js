9bd2be71552992837f76da9a7efcd2b8f97f0b82
Jenn <jenn@mixmax.com>
2015-04-25 21:19:46 -0700

    all the edits

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
 $(document).ready(function() {
-
   var imgCounter = 0;
   var audioCounter = 0;
 
   var createDiv = function (x, y) {
     var newDiv = $('<div>');
     var newImg = $('<img>');
-    var imgNumArray = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'];
+    var imgNumArray = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15'];
     var audioNumArray = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14'];
     var centerX = x - 100;
     var centerY = y - 100;
 
+    var newAudioName = 'developers-' + audioNumArray[audioCounter];
+    var newAudio = new Audio('mp3/' + newAudioName + '.mp3');
+    newAudio.play();
+
     var imgSrc = 'img/ballmer/png/ballmer-' + imgNumArray[imgCounter] + '.png';
 
     newImg.attr('src', imgSrc);
-    newImg.css({
-      height: '250px',
-      weight: '250px'
-    });
 
     newDiv.append(newImg);
     newDiv.css({
@@ -26,10 +25,6 @@ $(document).ready(function() {
       left: centerX + 'px'
     });
     $('body').append(newDiv);
-
-    var newAudioName = 'developers-' + audioNumArray[audioCounter];
-    var newAudio = new Audio('mp3/' + newAudioName + '.mp3');
-    newAudio.play();
 
     if (imgCounter == imgNumArray.length - 1) {
       imgCounter = 0;
@@ -44,9 +39,21 @@ $(document).ready(function() {
     }
   };
 
-  $(document).on('click', function (event) {
-    var x = event.pageX;
-    var y = event.pageY;
-    createDiv(x, y);
+  $('body').on('touchstart click', function (e) {
+    e.stopPropagation();
+    if (e.type == 'touchstart') {
+      var x = e.originalEvent.touches[0].pageX;
+      var y = e.originalEvent.touches[0].pageY;
+      createDiv(x, y);
+      alert(x + 'yes' + y);
+    } else if (e.type == 'click') {
+      var a = e.pageX;
+      var b = e.pageY;
+      createDiv(a, b);
+    }
+  });
+
+  $('footer').on('touchstart click', function (e) {
+    e.stopPropagation();
   });
 });
