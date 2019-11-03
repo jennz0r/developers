48a8e3838e05a511c8c092090a03133dd33dd7bf
Jennifer Wong <jenn.wong@modeanalytics.com>
2019-11-03 11:39:26 +0800

    add STT

--- index.js
+++ index.js
@@ -39,21 +39,99 @@ $(document).ready(function() {
     }
   };
 
-  $('body').on('touchstart click', function (e) {
-    e.stopPropagation();
-    if (e.type == 'touchstart') {
-      var x = e.originalEvent.touches[0].pageX;
-      var y = e.originalEvent.touches[0].pageY;
-      createDiv(x, y);
-      alert(x + 'yes' + y);
-    } else if (e.type == 'click') {
-      var a = e.pageX;
-      var b = e.pageY;
-      createDiv(a, b);
-    }
-  });
+  // $('body').on('touchstart click', function (e) {
+  //   e.stopPropagation();
+  //   if (e.type == 'touchstart') {
+  //     var x = e.originalEvent.touches[0].pageX;
+  //     var y = e.originalEvent.touches[0].pageY;
+  //     createDiv(x, y);
+  //     alert(x + 'yes' + y);
+  //   } else if (e.type == 'click') {
+  //     var a = e.pageX;
+  //     var b = e.pageY;
+  //     createDiv(a, b);
+  //   }
+  // });
 
   $('footer').on('touchstart click', function (e) {
     e.stopPropagation();
   });
+
+  $('.webspeech').on('touchstart click', function (e) {
+    e.stopPropagation();
+  });
+
+  var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+  var synth = window.speechSynthesis || 
+    webkitSpeechSynthesis;
+  var voiceSelect = document.getElementById("voices");
+  var voices = [];
+
+  function testSpeech(clickEvent) {
+    var diagnostic = document.querySelector('.output');
+    var recognition = new SpeechRecognition();
+    recognition.lang = voiceSelect.selectedOptions[0].getAttribute("data-lang");
+    recognition.start();
+
+    recognition.onresult = function(event) {
+      var speechResult = event.results[0][0].transcript;
+      // diagnostic.textContent = speechResult;
+
+      if (speechResult.toLowerCase().includes('developers')) {
+        var a = clickEvent.pageX;
+        var b = clickEvent.pageY;
+        createDiv(a, b);
+        say(speechResult);
+      }
+    }
+
+    recognition.onspeechend = function() {
+      recognition.stop();
+    }
+
+    recognition.onerror = function(event) {
+      diagnosticPara.textContent = 'Error occurred in recognition: ' + event.error;
+    }
+  }
+
+  document.body.onclick = function(clickEvent) {
+    testSpeech(clickEvent);
+  }
+
+  function say(text) {
+    var utterThis = new SpeechSynthesisUtterance(text);
+    var selectedOption = voiceSelect.selectedOptions[0].getAttribute("data-name");
+    for (let i = 0; i < voices.length; i++) {
+      if (voices[i].name === selectedOption) {
+        utterThis.voice = voices[i];
+      }
+    }
+    utterThis.pitch = 1;
+    utterThis.rate = 1;
+    synth.speak(utterThis);
+  }
+
+  function populateVoiceList() {
+    voices = synth.getVoices();
+    var selectedIndex = voiceSelect.selectedIndex < 0 ? 0 : voiceSelect.selectedIndex;
+    voiceSelect.innerHTML = "";
+    for(i = 0; i < voices.length ; i++) {
+      var option = document.createElement("option");
+      option.textContent = voices[i].name + " (" + voices[i].lang + ")";
+
+      if(voices[i].default) {
+        option.textContent += " -- DEFAULT";
+      }
+
+      option.setAttribute("data-lang", voices[i].lang);
+      option.setAttribute("data-name", voices[i].name);
+      voiceSelect.appendChild(option);
+    }
+    voiceSelect.selectedIndex = selectedIndex;
+  }
+
+  populateVoiceList();
+  if (speechSynthesis.onvoiceschanged !== undefined) {
+    speechSynthesis.onvoiceschanged = populateVoiceList;
+  }
 });
